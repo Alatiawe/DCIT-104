16b20b06ab93db000e62a47dc752032aff6d697b
Alatiawe <wboakye@st.ug.edu.gh>
2022-08-08 10:57:10 +0000

    first commit

---
+++ index.cpp
@@ -0,0 +1,26 @@
+//ID: 10996012
+//Name: William Boakye
+
+//C++ program to calculate the sum of all prime numbers less than a given number.
+
+#include <iostream>
+
+using namespace std;
+
+int main();
+
+bool is_prime(int a)
+{
+    int i;
+    for(i=2; i<a, i++)
+    {
+        if (m % i==0)
+        {
+            return false;
+        }
+    }
+    if (m % i!==0)
+    {
+        sum +=i;
+    }
+}
